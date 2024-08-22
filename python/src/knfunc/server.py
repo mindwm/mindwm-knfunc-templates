5b2f4a2531c1209058838480a6bc1674862594d7
Omg Bebebe <omgbebebe@gmail.com>
2024-08-22 08:08:38 +0000

    fix log_level names

--- python/src/knfunc/server.py
+++ python/src/knfunc/server.py
@@ -4,7 +4,7 @@ import os
 
 async def serve():
     port=os.getenv("PORT", 8080)
-    config = uvicorn.Config("knfunc.func:app", host="0.0.0.0", port=int(port), log_level=os.getenv("LOG_LEVEL", "INFO"))
+    config = uvicorn.Config("knfunc.func:app", host="0.0.0.0", port=int(port), log_level=os.getenv("LOG_LEVEL", "INFO").lower())
     server = uvicorn.Server(config)
     await server.serve()
 
